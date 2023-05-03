48d2f9cb594a9e23bb9da9dff654681f1ac68412
Ogundeyi Daniel Oluwatunmise <111064043+ogundeyidaniel@users.noreply.github.com>
2023-05-03 03:27:08 +0100

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,47 +1,32 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+"""app.py"""
 from models import storage
 from api.v1.views import app_views
+from werkzeug.exceptions import HTTPException
+from flask import Flask
 from os import getenv
-from flasgger import Swagger
-
+from flask import jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['SWAGGER'] = {"swagger_version": "2.0",
-        "title":"Flasgger","headers": [
-            ('Access-Control-Allow-Origin', '*'),
-            ('Access-Control-Allow-Methods',
-                "GET, POST, PUT, DELETE, OPTIONS"),
-            ('Access-Control-Allow-Credentials',
-                "true"),
-            ],
-        "specs": [
-            {"version": "1.0","title": "HBNB API",
-                "endpoint": 'v1_views',
-                "description": 'HBNB REST API',
-                "route": '/v1/views',
-                }
-            ]
-        }
-swagger = Swagger(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Closes storage session """
+def teardown_appcontext(error):
+    """teardown_appcontext"""
     storage.close()
 
+
 @app.errorhandler(404)
-def not_found(error):
-    """ Returns JSON response with 404 status """
-    return make_response(jsonify({"error":
-        "Not found"}), 404)
+def page_404(error):
+    """ Return a custom 404 error """
+    err_dict = {"error": "Not found"}
+    return jsonify(err_dict), 404
 
-if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
 
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
