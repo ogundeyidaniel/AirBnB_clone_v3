f02ad41a57db18a454c9a90a14c2ca8d079b7d6a
ogundeyidaniel <ogundeyidaniel@outlook.com>
2023-05-02 19:20:34 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,37 +10,33 @@ from flasgger import Swagger
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "title": "Flasgger",
-    "headers": [
-        ('Access-Control-Allow-Origin', '*'),
-        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
-        ('Access-Control-Allow-Credentials', "true"),
-    ],
-    "specs": [
-        {
-            "version": "1.0",
-            "title": "HBNB API",
-            "endpoint": 'v1_views',
-            "description": 'HBNB REST API',
-            "route": '/v1/views',
+app.config['SWAGGER'] = {"swagger_version": "2.0",
+        "title":"Flasgger","headers": [
+            ('Access-Control-Allow-Origin', '*'),
+            ('Access-Control-Allow-Methods',
+                "GET, POST, PUT, DELETE, OPTIONS"),
+            ('Access-Control-Allow-Credentials',
+                "true"),
+            ],
+        "specs": [
+            {"version": "1.0","title": "HBNB API",
+                "endpoint": 'v1_views',
+                "description": 'HBNB REST API',
+                "route": '/v1/views',
+                }
+            ]
         }
-    ]
-}
 swagger = Swagger(app)
-
 @app.teardown_appcontext
 def teardown_session(exception):
     """ Closes storage session """
     storage.close()
 
-
 @app.errorhandler(404)
 def not_found(error):
     """ Returns JSON response with 404 status """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+    return make_response(jsonify({"error":
+        "Not found"}), 404)
 
 if __name__ == '__main__':
     HBNB_API_HOST = getenv('HBNB_API_HOST')
